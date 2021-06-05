907da37aac36cc31889895d9d898b4bbb51202ca
zach <584191872@qq.com>
2021-06-05 17:48:46 +0800

    refactor: dispel warning and cancel print

--- test/test_cstl_vector.c
+++ test/test_cstl_vector.c
@@ -32,56 +32,61 @@ static void test_with_int()
 	for (i = 0; i < init_size; i++) {
 		ret = element_at_cstl_vector(my_vector, i, &elem);
 		assert(ret == CSTL_SUCCESS);
-		printf("after push_back_cstl_vector, my_vector->elements[%d]->data is %d\n", i, *(int*)elem);
+		assert(*(int*)elem == i);
+		//printf("after push_back_cstl_vector, my_vector->elements[%d]->data is %d\n", i, *(int*)elem);
 		free(elem);	
 	} 
 	
 	ret = front_cstl_vector(my_vector, &elem);
 	assert(ret == CSTL_SUCCESS);
-	printf("after push_back_cstl_vector, the front number is %d\n", *(int*)elem);
+	assert(*(int*)elem == 0);
+	//printf("after push_back_cstl_vector, the front number is %d\n", *(int*)elem);
 	free(elem);
 
 	ret = back_cstl_vector(my_vector, &elem);
-        assert(ret == CSTL_SUCCESS);
-        printf("after push_back_cstl_vector, the back number is %d\n", *(int*)elem);
+    assert(ret == CSTL_SUCCESS);
+	assert(*(int*)elem == init_size-1);
+    //printf("after push_back_cstl_vector, the back number is %d\n", *(int*)elem);
 	free(elem);
 
 	remove_from_cstl_vector(my_vector, 0);
 	assert(init_size - 1 == size_cstl_vector(my_vector));
 	ret = element_at_cstl_vector(my_vector, 0, &elem);
-	printf("after remove_from_cstl_vector(0), my_vector->elements[0]->data is %d\n", *(int*)elem);
+	assert(*(int*)elem == 1);
+	//printf("after remove_from_cstl_vector(0), my_vector->elements[0]->data is %d\n", *(int*)elem);
 	free(elem);
 
 	init_size = size_cstl_vector(my_vector);
-	printf("after remove_from_cstl_vector(0), init_size = %d\n", init_size);
+	assert(init_size == 19);
+	//printf("after remove_from_cstl_vector(0), init_size = %d\n", init_size);
 	remove_from_cstl_vector(my_vector, init_size/2);
 	assert(init_size -1 == size_cstl_vector(my_vector));
 	ret = element_at_cstl_vector(my_vector, init_size/2, &elem);
-	printf("after remove_from_cstl_vector(19/2), my_vector->element[9]->data is %d\n", *(int*)elem);
+	//printf("after remove_from_cstl_vector(19/2), my_vector->element[9]->data is %d\n", *(int*)elem);
 	assert(*(int*)elem == init_size/2 + 2);
 	free(elem);
 
 	init_size = size_cstl_vector(my_vector);
-	printf("after remove_from_cstl_vector(19/2), init_size = %d\n", init_size);
+	//printf("after remove_from_cstl_vector(19/2), init_size = %d\n", init_size);
 	remove_from_cstl_vector(my_vector, init_size - 1);
 	assert(init_size - 1 == size_cstl_vector(my_vector));
 	ret = element_at_cstl_vector(my_vector, init_size - 2, &elem);
-	printf("after remove_from_cstl_vector(18-1), my_vector->element[16]->data is %d\n", *(int*)elem);
+	//printf("after remove_from_cstl_vector(18-1), my_vector->element[16]->data is %d\n", *(int*)elem);
 	assert(*(int*)elem == 18);
 	free(elem);
 
 	i = 888;
 	insert_at_cstl_vector(my_vector, 10, &i, sizeof(int));
 	ret = element_at_cstl_vector(my_vector, 10, &elem);
-	printf("after insert_at_cstl_vector(10, 888), my_vector->element[10]->data is %d\n", *(int*)elem);
-	printf("my_vector->element[10]->data is %d\n", *(int*)my_vector->elements[10]->data);
+	//printf("after insert_at_cstl_vector(10, 888), my_vector->element[10]->data is %d\n", *(int*)elem);
+	//printf("my_vector->element[10]->data is %d\n", *(int*)my_vector->elements[10]->data);
 	assert(*(int*)elem == i);
 	free(elem);	
 
 	for (i = 0; i < init_size; i++) {
 		ret = element_at_cstl_vector(my_vector, i, &elem);
 		assert(ret == CSTL_SUCCESS);
-		printf("now, my_vector->elements[%d]->data is %d\n", i, *(int*)elem);
+		//printf("now, my_vector->elements[%d]->data is %d\n", i, *(int*)elem);
 		free(elem);
         }
 	delete_cstl_vector(my_vector);
@@ -109,21 +114,25 @@ static void test_with_pointers()
 	for (i = 0; i <size; i++) {
 		ret = element_at_cstl_vector(my_vector, i, &elem);
 		assert(CSTL_SUCCESS == ret);
-		printf("after push_back_cstl_vector, my_vector->elements[%d]->raw_data = %d\n", i, *(int *)elem);
+		assert(*(int*)elem == i);
+		//printf("after push_back_cstl_vector, my_vector->elements[%d]->data = %d\n", i, *(int *)elem);
 		free(elem);
 	}
 
 	ret = front_cstl_vector(my_vector, &elem);
 	assert(CSTL_SUCCESS == ret);
-	printf("after push_back_cstl_vector, my_vector->elements[0]->raw_data = %d\n", *(int *)elem);	
+	assert(*(int*)elem == 0);
+	//printf("after push_back_cstl_vector, my_vector->elements[0]->data = %d\n", *(int *)elem);	
 	free(elem);
 	
 	ret = back_cstl_vector(my_vector, &elem);
 	assert(CSTL_SUCCESS == ret);
-	printf("after push_bacl_cstl_vector, my_vector->elements[9]->raw_data = %d\n", *(int *)elem);
+	assert(*(int*)elem == size-1);
+	//printf("after push_bacl_cstl_vector, my_vector->elements[9]->data = %d\n", *(int *)elem);
 	free(elem);	
 
-	printf("after push_back_cstl_vector, my_vector->capacity = %d\n", capacity_cstl_vector(my_vector));
+	assert(capacity_cstl_vector(my_vector) >= size);
+	//printf("after push_back_cstl_vector, my_vector->capacity = %d\n", capacity_cstl_vector(my_vector));
 	
 	ret = remove_from_cstl_vector(my_vector, 0);
 	assert(ret == CSTL_SUCCESS);
@@ -143,7 +152,7 @@ static void test_with_pointers()
 	for (i = 0; i < size; i++) {
 		ret = element_at_cstl_vector(my_vector, i, &elem);
 		assert(CSTL_SUCCESS == ret);
-		printf("now, my_vector->elements[%d]->raw_data = %d\n", i, *(int *)elem);
+		//printf("now, my_vector->elements[%d]->raw_data = %d\n", i, *(int *)elem);
 		free(elem);
 
 	}
@@ -169,9 +178,12 @@ static void test_with_strings()
 	}
 
 	for (i = 0; i < size; i++) {
+		/*
 		char *v = strdup(input_vector[i]);
 		push_back_cstl_vector(my_vector, v, strlen(v) + 1);
 		free(v);
+		*/
+		push_back_cstl_vector(my_vector, input_vector[i], sizeof(input_vector[i]));
 	}
 	assert(cstl_false == empty_cstl_vector(my_vector));
 	assert(size == size_cstl_vector(my_vector));
@@ -180,21 +192,21 @@ static void test_with_strings()
 		ret = element_at_cstl_vector(my_vector, i, &elem);
 		assert(ret == CSTL_SUCCESS);
 		assert(strcmp((char *)elem, input_vector[i]) == 0);
-		printf("after push_back_cstl_vector, my_vector->elements[%d]->raw_data = %s\n", i, (char *)elem);
+		//printf("after push_back_cstl_vector, my_vector->elements[%d]->raw_data = %s\n", i, (char *)elem);
 		free(elem);
 	}
 
 	ret = front_cstl_vector(my_vector, &elem);
 	assert(CSTL_SUCCESS == ret);
-	printf("after push_back_cstl_vector, my_vector->elements[0]->raw_data = %s\n", (char *)elem);	
+	//printf("after push_back_cstl_vector, my_vector->elements[0]->raw_data = %s\n", (char *)elem);	
 	free(elem);
 	
 	ret = back_cstl_vector(my_vector, &elem);
 	assert(CSTL_SUCCESS == ret);
-	printf("after push_bacl_cstl_vector, my_vector->elements[9]->raw_data = %s\n", (char *)elem);
+	//printf("after push_bacl_cstl_vector, my_vector->elements[9]->raw_data = %s\n", (char *)elem);
 	free(elem);	
 
-	printf("after push_back_cstl_vector, my_vector->capacity = %d\n", capacity_cstl_vector(my_vector));
+	//printf("after push_back_cstl_vector, my_vector->capacity = %d\n", capacity_cstl_vector(my_vector));
 	
 	ret = remove_from_cstl_vector(my_vector, 0);
 	assert(ret == CSTL_SUCCESS);
@@ -211,11 +223,11 @@ static void test_with_strings()
 	for (i = 0; i < size; i++) {
 		ret = element_at_cstl_vector(my_vector, i, &elem);
 		assert(CSTL_SUCCESS == ret);
-		printf("now, my_vector->elements[%d]->raw_data = %s\n", i, (char *)elem);
+		//printf("now, my_vector->elements[%d]->raw_data = %s\n", i, (char *)elem);
 		free(elem);
 	}
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size + 1; i++)
 		free(input_vector[i]);
 	delete_cstl_vector(my_vector);
 }
@@ -229,7 +241,7 @@ static void print_using_iterators(struct cstl_vector *my_vector)
 	element = my_itr->get_next(my_itr);
 	while (element) {
 		void *value = my_itr->get_value(element);
-		printf("%d\n", *(int *)value);
+		//printf("%d\n", *(int *)value);
 		free(value);
 		element = my_itr->get_next(my_itr);
 	}
@@ -256,8 +268,6 @@ static void test_with_iterator()
 {
 	int size = 10;
 	int i = 0;
-	int ret = CSTL_SUCCESS;
-	void *elem = (void *)0;
 	struct cstl_vector *my_vector = new_cstl_vector(8, compare_function, NULL);
 	for (i = 0; i < size; i++)
 		push_back_cstl_vector(my_vector, &i, sizeof(int));
